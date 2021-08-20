0d09cdb13baea9666e48b39ea0151a30777f7db7
Emilie <milogare@yahoo.fr>
2021-08-20 15:08:12 +0200

    Removed default app.component.html content and Changed app title and spec accordingly

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'test-front-auth-angular'`, () => {
+  it(`should have as title 'Implémentation des tests en FRONT sur la logique métier d'authentification'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-front-auth-angular');
+    expect(app.title).toEqual('TDD avec Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('test-front-auth-angular app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('TDD avec Angular');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-front-auth-angular';
+  title = 'TDD avec Angular';
 }
